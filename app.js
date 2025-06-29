d829e091c8e9903bca81b2e1da395c9e23706e35
adarshp2810 <121097943+adarshp2810@users.noreply.github.com>
2025-06-29 17:36:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ const server = http.createServer((req, res) => {
   res.end('Hello from AKS Pipeline! 🚀\n');
 });
 
-server.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
+server.listen(port, '0.0.0.0', () => {
+  console.log(`Server running at http://0.0.0.0:${port}/`);
 });
